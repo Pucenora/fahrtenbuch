65b7adfb6c3ed6fa69c97cf67fec9a60a18a8050
Jan Kowalke <jan@kowalke@web.de>
2015-04-02 15:38:25 +0200

    dummy form built

--- client/app/trip/trip.controller.js
+++ client/app/trip/trip.controller.js
@@ -1,6 +1,11 @@
-// 'use strict';
+'use strict';
 
-// angular.module('fahrtenbuchApp')
-//   .controller('TripCtrl', function ($scope, $http, socket) {
-//     $scope.awesomeThings = [];
-//   });
+angular.module('fahrtenbuchApp').controller('TripCtrl', function ($scope, $http, socket) {
+
+  $scope.items = [
+    'Sparda',
+    'Dresdner',
+    'Sparkasse'
+  ];
+
+});
