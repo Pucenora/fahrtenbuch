251d615d0a2eb4a43e972ed5aba07622853c36d9
Jan Kowalke <jan@kowalke@web.de>
2015-04-20 14:12:37 +0200

    removed unneeded code

--- client/app/trip/trip.controller.js
+++ client/app/trip/trip.controller.js
@@ -8,13 +8,11 @@ angular.module('fahrtenbuchApp')
 
     $http.get('/api/trips').success(function(trips) {
       $scope.trips = trips;
-      socket.syncUpdates('trip', $scope.trips);
     });
 
 		$scope.addTrip = function() {
 			$location.path("/trip/new");
 		}	
-
 	})	
 
 	.controller('CreateTripCtrl', function ($scope, $http, socket, $location) {
@@ -36,11 +34,8 @@ angular.module('fahrtenbuchApp')
 
 		$scope.addTrip = function() {
 
-			//
 			$scope.trip.account = $scope.trip.account._id;
-			
-			var json_data = JSON.stringify($scope.trip);
-			$http.post('/api/trips', json_data);
+			$http.post('/api/trips', $scope.trip);
 
 			// redirect
 			$location.path("/trip");
@@ -75,39 +70,13 @@ angular.module('fahrtenbuchApp')
 		$scope.trip = {};
 
 		var tripURL = '/api/trips/' + $routeParams.id;
-		console.log(tripURL);
 
     $http.get(tripURL).success(function(trip) {
       $scope.trip = trip;
-      console.log(trip);
-      socket.syncUpdates('trip', $scope.trip);
     });
 
     $scope.returnToOverview = function() {
 			$location.path("/trip");
 		};
 	})
-
-  // .directive('reverseGeocode', function () {
-  //     return {
-  //         restrict: 'E',
-  //         template: '<div></div>',
-  //         link: function (scope, element, attrs) {
-  //             var geocoder = new google.maps.Geocoder();
-  //             var latlng = new google.maps.LatLng(attrs.lat, attrs.lng);
-  //             geocoder.geocode({ 'latLng': latlng }, function (results, status) {
-  //                 if (status == google.maps.GeocoderStatus.OK) {
-  //                     if (results[1]) {
-  //                         element.text(results[1].formatted_address);
-  //                     } else {
-  //                         element.text('Location not found');
-  //                     }
-  //                 } else {
-  //                     element.text('Geocoder failed due to: ' + status);
-  //                 }
-  //             });
-  //         },
-  //         replace: true
-  //     }
-  // })
 ;
