87b2ece2c33dda7bdabf51ac4765385373047fed
Jan Kowalke <jan@kowalke@web.de>
2015-04-21 17:03:34 +0200

    added comments to trip.controller

--- client/app/trip/trip.controller.js
+++ client/app/trip/trip.controller.js
@@ -2,23 +2,35 @@
 
 angular.module('fahrtenbuchApp')
 
+	/**
+	 * /trip
+	 * trip overview 
+	**/
 	.controller('TripCtrl', function ($scope, $http, socket, $location) {
 	
-	$scope.trips = [];
-
+		// get trips
+		$scope.trips = [];
     $http.get('/api/trips').success(function(trips) {
       $scope.trips = trips;
     });
 
+    // redirect
 		$scope.addTrip = function() {
 			$location.path("/trip/new");
 		}	
 	})	
 
+	/**
+	 * /trip/new
+	 * create new trip
+	**/
 	.controller('CreateTripCtrl', function ($scope, $http, socket, $location, Auth) {
 
+		// init
 		$scope.hourStep = 1;
   	$scope.minuteStep = 5;
+  	$scope.trip.origin_time = new Date();
+		$scope.trip.destination_time = new Date();
 
 	 	$scope.accounts = [];
 	 	$scope.cars = [];
@@ -28,9 +40,9 @@ angular.module('fahrtenbuchApp')
 	 	$scope.stays = [];
 	 	$scope.stays.push({destination: '', client: '', destination_time: new Date()});
 
+	 	// get data
 	  $http.get('/api/accounts').success(function(accounts) {
 	    $scope.accounts = accounts;
-		  // $scope.trip.account = $scope.accounts[0]; 
 	  });
 
 	  $http.get('/api/cars').success(function(cars) {
@@ -40,23 +52,31 @@ angular.module('fahrtenbuchApp')
 	  var carURL = '/api/cars/' + $scope.user.default_car;
 
 	  $http.get(carURL).success(function(car) {
-		  // $scope.trip.car = car;
 		  $scope.trip.car = $scope.cars[car.__v];
 		  $scope.trip.kilometer_start = car.mileage;
 	  });
 
-	  $scope.trip.origin_time = new Date();
-		$scope.trip.destination_time = new Date();
-
+	  // @todo
 		// var Geocoder = new google.maps.Geocoder();
 
+		/**
+		 * add a stay to the form
+		**/
 		$scope.addStay = function() {
 			$scope.stays.push({destination: '', client: '', destination_time: new Date()});
 		};
 
+		/**
+		 * post trip to server
+		**/
 		$scope.addTrip = function() {
 
+			// init
 			var stayIds = [];
+			var len = $scope.stays.length;
+			var counter = 0;
+
+			// add object ids to trip and post trip
 			if($scope.trip.account) {
 				$scope.trip.account = $scope.trip.account._id;
 			}
@@ -64,12 +84,8 @@ angular.module('fahrtenbuchApp')
 			$scope.trip.user = $scope.user._id;
 			$scope.trip.timestamp = new Date();
 
-			var len = $scope.stays.length;
-			var counter = 0;
-
 			$scope.stays.forEach(function(stay) {
-				$http.post('/api/stays', stay).success(function(data, status, headers, config) {
-  				// console.log(data._id);  				
+				$http.post('/api/stays', stay).success(function(data, status, headers, config) {		
   				stayIds.push(data._id);
   				counter += 1;
   				if (counter === len) {
@@ -85,11 +101,19 @@ angular.module('fahrtenbuchApp')
 			$location.path("/trip");
 		};
 
+		/**
+		 * @todo
+		 * log lat und lng
+		**/
 		$scope.showPosition = function(position) {
 	    console.log("Latitude: " + position.coords.latitude);
 	    console.log("Longitude: " + position.coords.longitude);
 		}
 
+		/**
+		 * @todo
+		 * start getting location
+		**/
 		$scope.startWatching = function() {
 	    if (navigator.geolocation) {
 	    		console.log(new Date());
@@ -101,6 +125,10 @@ angular.module('fahrtenbuchApp')
 	    }
 		}
 
+		/**
+		 * @todo
+		 * stop getting location
+		**/
 		$scope.stopWatching = function() {
 			console.log(new Date());
 			var latLngObject = navigator.geolocation.getCurrentPosition($scope.showPosition);
@@ -109,16 +137,21 @@ angular.module('fahrtenbuchApp')
 		}
 	})
 
+	/**
+	 * show details of a trip
+	**/
 	.controller('DetailTripCtrl', function ($scope, $http, $routeParams, socket, $location) {
 
+		// init
 		$scope.trip = {};
 
+		// get trip from server
 		var tripURL = '/api/trips/' + $routeParams.id;
-
     $http.get(tripURL).success(function(trip) {
       $scope.trip = trip;
     });
 
+    // redirect
     $scope.returnToOverview = function() {
 			$location.path("/trip");
 		};
