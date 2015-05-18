7a93f7243a03b227e322a78e159c4f942f271977
Jan Kowalke <jan@kowalke@web.de>
2015-05-18 09:44:44 +0200

    trip detail test now work

--- client/app/trip/trip.controller.spec.js
+++ client/app/trip/trip.controller.spec.js
@@ -24,5 +24,36 @@ describe('Controller: TripCtrl', function () {
 	}));
 });
 
+// CreateTripCtrl
+
+describe('Controller: DetailTripCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('fahrtenbuchApp'));
+  var DetailTripCtrl, scope, $httpBackend, testTrip;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope, $routeParams) {
+    
+    $httpBackend = _$httpBackend_;
+    scope = $rootScope.$new();
+    DetailTripCtrl = $controller('DetailTripCtrl', {
+      $scope: scope,
+      $routeParams: {id: 12345678}
+    });
+    testTrip = {_id: 12345678, stays: [{destination: "test"}]};
+  }));
+
+  it('should get trips', inject(function() {
+
+    $httpBackend.expectGET('/api/trips/' + testTrip._id).respond(testTrip);
+    $httpBackend.flush();
+
+    expect(scope.trip).toEqual({_id: 12345678, stays: [{destination: "test"}]});
+    expect(scope.destinations).toEqual("test");
+  }));
+});
+
+
 
 
