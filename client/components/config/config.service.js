1f30c98ff755fa7691a3a65d2ae3df077064c812
Jan Kowalke <jan@kowalke@web.de>
2015-07-07 10:55:57 +0200

    added config file or rather service

---
+++ client/components/config/config.service.js
@@ -0,0 +1,27 @@
+'use strict';
+/* global google */
+/* global navigator */
+
+angular.module('fahrtenbuchApp')
+  .factory('config', function config() {
+    return {
+      defaultLocalizationAccuracy : 10,
+      desktopLocalizationAccuracy : 10000,
+      defaultLocalizationOptions : {
+			  enableHighAccuracy: true,
+			  timeout: 5000,
+			  maximumAge: 0
+			},
+			defaultMapOptions : {
+				zoom: 20,
+	 			// center: 'Augsburg',
+	 			streetViewControl: false,
+	 			mapTypeControl: false,
+	 			scrollwhell: false,
+	 			navigationControlOptions: {
+					style: google.maps.NavigationControlStyle.SMALL
+	 			},
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			}
+    };
+  });
