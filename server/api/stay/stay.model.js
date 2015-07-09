91b6ee50b4a4d57b3310d609dcb299c67bb47479
Jan Kowalke <jan@kowalke@web.de>
2015-07-09 13:11:52 +0200

    changed stay model

--- server/api/stay/stay.model.js
+++ server/api/stay/stay.model.js
@@ -4,9 +4,16 @@ var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
 var StayShema = new Schema({
-	destination: String,
-	client: String,
-	destinationTime: Date,
+	// destination: String,
+	destination: {type: String, required: true},
+	destinationLong: {type: Number, required: false},
+	destinationLat: {type: Number, required: false},
+	// destinationLong: Number,
+	// destinationLat: Number,
+	// client: String,
+	// destinationTime: Date,
+	client: {type: String, required: false},
+	destinationTime: {type: Date, required: true}
 });
 
 module.exports = mongoose.model('Stay', StayShema);
